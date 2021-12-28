a03237e7ccb47cea3d60568203467db438033cd4
Razee <razee.hj@gmail.com>
2021-12-28 15:31:16 +0300

    Time: 156 ms (35.81%), Space: 18.1 MB (80.98%) - LeetHub

---
+++ reverse-string/reverse-string.swift
@@ -0,0 +1,14 @@
+class Solution {
+    func reverseString(_ s: inout [Character]) {
+        var left = 0
+        var right = s.count - 1
+        
+        while left <= right {
+            var temp = s[left]
+            s[left] = s[right]
+            s[right] = temp
+            left += 1
+            right -= 1 
+        }
+    }
+}
