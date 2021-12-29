f46f99eca45fb7af13c33bd24c904b33b65e1ccd
Razee <razee.hj@gmail.com>
2021-12-29 10:38:35 +0300

    Time: 43 ms (5.42%), Space: 15 MB (52.22%) - LeetHub

---
+++ odd-even-linked-list/odd-even-linked-list.swift
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+class Solution {
+    func oddEvenList(_ head: ListNode?) -> ListNode? {
+        var odd: ListNode? = head, even: ListNode? = head?.next, evenhead = even
+        
+        while (even != nil && even?.next != nil) {
+            odd?.next = even?.next
+            odd = odd?.next
+            even?.next = odd?.next
+            even = even?.next
+        }
+        
+        odd?.next = evenhead
+        return head
+    }
+}
