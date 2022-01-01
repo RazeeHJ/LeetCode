e6b19bb915bd7a97a30b69af39ee37a45de4a9ad
Razee <razee.hj@gmail.com>
2022-01-01 21:45:44 +0300

    Time: 0 ms (100.00%), Space: 14 MB (31.72%) - LeetHub

--- n-th-tribonacci-number/n-th-tribonacci-number.swift
+++ n-th-tribonacci-number/n-th-tribonacci-number.swift
@@ -4,18 +4,23 @@ class Solution {
             return 0
         }
         
-        if n == 1 || n == 2 {
+        if n == 1 {
             return 1
         }
         
-        var arr = [Int](repeating: 0, count: n+1)
-        arr[0] = 0
-        arr[1] = 1
-        arr[2] = 1
-        for i in 3...n {
-            arr[i] = arr[i-3] + arr[i-2] + arr[i-1]
+        if n == 2 {
+            return 1
         }
         
-        return arr[n]
+        var result = [Int](repeating: 0, count: n+1)
+        result[0] = 0
+        result[1] = 1
+        result[2] = 1
+        
+        for i in 3...n {
+            result[i] = result[i-3] + result[i-2] + result[i-1]
+        }
+        
+        return result[n]
     }
 }
