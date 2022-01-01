411a1e22738f8a603a61bdac8e1a4f30ea5a9309
Razee <razee.hj@gmail.com>
2022-01-01 22:09:18 +0300

    Time: 836 ms (68.27%), Space: 17.9 MB (99.59%) - LeetHub

--- maximum-subarray/maximum-subarray.swift
+++ maximum-subarray/maximum-subarray.swift
@@ -1,27 +1,16 @@
-// nums = [-2,1,-3,4,-1,2,1,-5,4]
-// index | currentSub | Sum |
-//   0         -2        -2
-//   1          1         1
-//   2         -2         1
-//   3          4         4
-//   4          3         4
-//   5          5         5
-//   6          6         6
-//   7          1         6
-//   8          5         6  
 class Solution {
     func maxSubArray(_ nums: [Int]) -> Int {
-        var currentSub = nums[0]
         var sum = nums[0]
+        var currentSub = nums[0]
         
         for i in 1..<nums.count {
-            if (nums[i] > currentSub + nums[i]) {
-                currentSub = nums[i]
-            } else {
+            if nums[i] <= nums[i] + currentSub {
                 currentSub += nums[i]
+            } else {
+                currentSub = nums[i]
             }
             
-            if (currentSub > sum) {
+            if currentSub > sum {
                 sum = currentSub
             }
         }
