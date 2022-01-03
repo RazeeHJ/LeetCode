d9a3f5e9e28412c4a8a100fdbf50ca8b3631d242
Razee <razee.hj@gmail.com>
2022-01-03 13:08:15 +0300

    Time: 1482 ms (10.69%), Space: 17.4 MB (94.99%) - LeetHub

--- best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.swift
+++ best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.swift
@@ -1,18 +1,13 @@
-// prices = [7,1,5,3,6,4]
-// buyingPrice | price | maxProfit
-//      7          1         0
-//      1          5         4
-//      1          6 
 class Solution {
     func maxProfit(_ prices: [Int]) -> Int {
         var buyingPrice = prices[0]
         var maxProfit = 0
         
-        for i in 1..<prices.count {
-            if prices[i] < buyingPrice {
-                buyingPrice = prices[i]
-            } else if (prices[i] - buyingPrice > maxProfit) {
-                maxProfit = prices[i] - buyingPrice
+        for (index, value) in prices.enumerated() {
+            if (buyingPrice > value) {
+                buyingPrice = value
+            } else if (value - buyingPrice > maxProfit) {
+                maxProfit = value-buyingPrice
             }
         }
         
