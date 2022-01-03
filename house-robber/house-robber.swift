8874c6bd0fabbb3430bf5abbb282bcd8e48ae3a7
Razee <razee.hj@gmail.com>
2022-01-03 13:22:14 +0300

    Time: 4 ms (80.16%), Space: 14.2 MB (21.81%) - LeetHub

--- house-robber/house-robber.swift
+++ house-robber/house-robber.swift
@@ -1,16 +1,17 @@
 class Solution {
     func rob(_ nums: [Int]) -> Int {
-        guard nums.count > 1 else {return nums[0]}
-        var sizeOfArray = nums.count - 1
-        var arr: [Int] = [Int](repeating: 0, count: sizeOfArray+1)
+        guard nums.count > 1 else { return nums[0]}
         
-        // Base cases
+        var arr = [Int](repeating: 0, count: nums.count)
+        
         arr[0] = nums[0]
         arr[1] = max(nums[0], nums[1])
         
         for i in 2..<nums.count {
-            arr[i] = max(arr[i-1], arr[i-2]+nums[i])
+            var max = max(arr[i-1], arr[i-2]+nums[i])
+            arr[i] = max
         }
-        return arr[sizeOfArray]
+        
+        return arr[nums.count-1]
     }
 }
