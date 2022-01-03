b7329483812c1c4ecdb1bf650a7cf94bfba6e19b
Razee <razee.hj@gmail.com>
2022-01-03 12:09:55 +0300

    Time: 31 ms (8.72%), Space: 14.1 MB (75.58%) - LeetHub

--- intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.swift
+++ intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.swift
@@ -1,37 +1,22 @@
 class Solution {
     func intersect(_ nums1: [Int], _ nums2: [Int]) -> [Int] {
-        let sortedNums1 = nums1.sorted()
-        let sortedNums2 = nums2.sorted()
+        let sorted1 = nums1.sorted()
+        let sorted2 = nums2.sorted()
 
-        var indx = 0
-        if sortedNums1.count > sortedNums2.count {
-            indx = sortedNums2.count
-        } else {
-            indx = sortedNums1.count
-        }
-        var arr: [Int] = []
-
-        var startIndx = 0
-        var left = 0
-        var right = 0
+        var indx1 = 0
+        var indx2 = 0
         
-        while (startIndx < indx) {
-            let l = left < sortedNums1.count ? sortedNums1[left] : -1
-            let r = right < sortedNums2.count ? sortedNums2[right] : -1
-            
-            if (l == -1 || r == -1) {
-                return arr
-            }
-            
-            if (l == r ) {
-                arr.append(l)
-                left += 1
-                right += 1
-                startIndx += 1
-            } else if (l < r ) {
-                left += 1
-            } else if (r < l) {
-                right += 1
+        var arr = [Int]()
+        
+        while (indx1 < sorted1.count && indx2 < sorted2.count) {
+            if (sorted1[indx1] == sorted2[indx2]) {
+                arr.append(sorted1[indx1])
+                indx1 += 1
+                indx2 += 1
+            } else if (sorted1[indx1] < sorted2[indx2]) {
+                indx1 += 1
+            } else {
+                indx2 += 1
             }
         }
         
